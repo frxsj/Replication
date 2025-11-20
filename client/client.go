ce431c144aaa542ced4edd02a435d7cc85837cb6
Camilla <cfjo@itu.dk>
2025-11-20 12:37:13 +0100

    so far so good

--- client/client.go
+++ client/client.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	proto "ITU/grpc"
+	"bufio"
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"strings"
+	"strconv"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -11,24 +16,76 @@ import (
 
 type gangClient struct {
 	client proto.GangnameStyleClient
+	id string
+	
 }
 
 func main() {
-	connectClient()
+	gangClient{
+		client : connectClient()
+	}  // How do we randomize the ID?
+
+	reader := bufio.NewReader(os.Stdin)
+	gangClient.client.initiateAuctionTalk(reader)
 
 }
 
-func connectClient() {
-	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func (gang *GangnameStyleClient) initiateAuctionTalk(reader *bufio.Reader) {
+	fmt.Println("Welcome to the auction!")
+	fmt.Println("Today's item is: one (1) Arla Cultura, strawberry flavor")
+	fmt.Println("Type /bid and the amount you would like to bid on the item!")
+	fmt.Println("Type /result to get the current status of the auction.")
+
+	for {
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		if message == "/result" {
+			plsres := &proto.PlsResult{
+				ClientID: gang.id,
+			}
+			gang.Result(context.Background(),plsres)
+		}
+		
+		if strings.HasPrefix(message, "/bid") {
+			var bid = strings.Split(message, " ")
+			if(len(bid)> 2) {
+				fmt.Println("[Failure] Too many words.")
+			} else if(len(bid) <= 1) {
+				fmt.Println("[Failure] Don't forget the amount!")
+			} else {
+				amount, err := strconv.ParseInt(bid[1], 10, 64) // String, Base 10, int64
+				if(err != nil) {
+					fmt.Println("You cant do that my drilla.")
+					return
+				}
+				plsBid := &proto.Request{
+					ClientID : gang.id,
+					Amount : amount,
+				}
+				response, err := gang.client.Bid(context.Background(), plsBid)
+				if err != nil {
+					fmt.Println("[Client] Failed to send!")
+				}
+				return response
+			}
+
+		}
+
+	}
+
+}
+
+func connectClient()(proto.NewGangnameStyleClient) {
+	conn, err := grpc.NewClient("localhost:5005", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Not working")
 	}
 
 	client := proto.NewGangnameStyleClient(conn)
-
+	return client
 }
 
-func (c *gangnameStyleClient) SendRequest(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-
+func (c *gangClient) Result(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
 	return &proto.Response{}, nil
 }
--- node/node.go
+++ node/node.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	proto "ITU/grpc"
+	"context"
+	"net"
+	"log"
+	
+
+	"google.golang.org/grpc"
 )
 
 //Leader-based implementation, nodes are replicas
@@ -10,23 +16,52 @@ import (
 type gangnamStyle struct {
 	proto.UnimplementedGangnameStyleServer
 	amIsILeader bool           //Which replica is the leader
-	wolfPack    []gangnamStyle //List of all replicas
-
 }
+
+var wolfPack []gangnamStyle // Global slice of all nodes
 
 func main() {
-
+	server := &gangnamStyle{}
+	
+	log.Println("[Server] Starting server...")
+	server.start_server(":5005")
+	wolfPack = append(wolfPack, *server)
 }
 
-func (gangy *gangnamStyle) selectGangLeader() { // Handles leader failure.
+func (gangy *gangnamStyle) forwardToAlpha(req *proto.Request) {
+	
+}
+
+
+func selectGangLeader(wolfPack []gangnamStyle) { // Handles leader failure.
 	var isAlphaExist bool // Does leader exist
-	for i := 0; i < len(gangy.wolfPack); i++ {
-		if gangy.wolfPack[i].amIsILeader { // Goes through the whole list of nodes
+	for i := 0; i < len(wolfPack); i++ {
+		if wolfPack[i].amIsILeader { // Goes through the whole list of nodes
 			isAlphaExist = true // If the leader is still working the for loop breaks.
 			break
 		}
 	}
 	if !isAlphaExist {
-		gangy.wolfPack[0].amIsILeader = true // Sets first in list as new leader
+		wolfPack[0].amIsILeader = true // Sets first in list as new leader
 	}
 }
+
+func (gangy *gangnamStyle) start_server(port string){
+	node := grpc.NewServer()
+	listener, err := net.Listen("tcp", port)
+	if(err != nil){
+		log.Fatalf("Did not work")
+	}
+
+	proto.RegisterGangnameStyleServer(node, gangy)
+
+	log.Println("Server startup was successful at port" + port)
+	
+	err = node.Serve(listener)
+}
+
+func (gangy *gangnamStyle) bid (ctx context.Context, req *proto.Request){
+
+}
+
+func(gangy gangnamStyle) result (ctx context.Context, req *proto.PlsResult)
